54b75a7f123c20e85ed3538e73875dc4b2cec6ec
hashrock <hashrock@users.noreply.github.com>
2023-11-22 20:12:16 +0900

    update

--- kv-connect/read.ts
+++ kv-connect/read.ts
@@ -2,9 +2,28 @@ import { encodeBinary as encodeAtomicWrite } from './proto/messages/com/deno/kv/
 import { encodeBinary as encodeSnapshotRead } from './proto/messages/com/deno/kv/datapath/SnapshotRead.ts';
 import { decodeBinary as decodeSnapshotReadOutput } from './proto/messages/com/deno/kv/datapath/SnapshotReadOutput.ts';
 import { decodeBinary as decodeAtomicWriteOutput } from './proto/messages/com/deno/kv/datapath/AtomicWriteOutput.ts';
-import { AtomicWrite, AtomicWriteOutput, SnapshotRead, SnapshotReadOutput } from './proto/messages/com/deno/kv/datapath/index.ts';
+import { AtomicWrite, AtomicWriteOutput, ReadRange, SnapshotRead, SnapshotReadOutput } from './proto/messages/com/deno/kv/datapath/index.ts';
+import { packKey } from "https://raw.githubusercontent.com/skymethod/kv-connect-kit/9871a9f4e3014a8307575eb8e439170d0e0a2446/src/kv_key.ts"
 
-const protobuf = "" //TODO
+const start = packKey(["foo"])
+const end = packKey(["bar"])
+
+const range: ReadRange = {
+  start,
+  end,
+  limit: 10,
+  reverse: false,
+}
+
+const snapshotRead: SnapshotRead = {
+  ranges: [
+    range
+  ]
+}
+
+const protobuf = encodeSnapshotRead(snapshotRead)
+
+console.log(protobuf)
 
 const res = await fetch("http://0.0.0.0:4512/v2/snapshot_read", {
   method: "POST",
@@ -17,3 +36,5 @@ const res = await fetch("http://0.0.0.0:4512/v2/snapshot_read", {
   body: protobuf,
 });
 
+console.log(res.status)
+console.log(res.statusText)
